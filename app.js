68ea24c87e3b566618c4ee2b3616935dfc71a498
dev-gaurav <gaurav.singh@flairminds.com>
2025-05-04 19:07:27 +0530

    vercel added

--- app.js
+++ app.js
@@ -8,13 +8,26 @@ const serverless = require('serverless-http');
 const app = express();
 app.use(cors({
     origin: "https://sisimanu-web.vercel.app",
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    credentials: true
 }))
-app.options('*', cors());
+app.options('*', cors({
+    origin: 'https://sisimanu-web.vercel.app',
+    credentials: true
+}));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://sisimanu-web.vercel.app');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    // mirror the methods & headers you expect:
+    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
 // convert JSON into js object so that node can read
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
 const authRouter = require('./routes/auth')
@@ -24,6 +37,8 @@ const userRouter = require('./routes/user');
 const initializeSocket = require('./utils/socket');
 const { chatRouter } = require('./routes/chat');
 const { paymentRouter } = require('./routes/payment');
+
+
 
 app.use("/", authRouter)
 app.use("/", profileRouter)
