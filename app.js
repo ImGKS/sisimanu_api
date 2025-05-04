1fd2528b1311fca476c684f912ef00da694388d8
dev-gaurav <gaurav.singh@flairminds.com>
2025-05-04 22:38:59 +0530

    bug fix

--- app.js
+++ app.js
@@ -10,20 +10,6 @@ app.use(cors({
     origin: "https://sisimanu-web.vercel.app",
     credentials: true
 }))
-app.options('*', cors({
-    origin: 'https://sisimanu-web.vercel.app',
-    credentials: true
-}));
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://sisimanu-web.vercel.app');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    // mirror the methods & headers you expect:
-    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
 
 // convert JSON into js object so that node can read
 app.use(express.json())
@@ -37,8 +23,6 @@ const userRouter = require('./routes/user');
 const initializeSocket = require('./utils/socket');
 const { chatRouter } = require('./routes/chat');
 const { paymentRouter } = require('./routes/payment');
-
-
 
 app.use("/", authRouter)
 app.use("/", profileRouter)
